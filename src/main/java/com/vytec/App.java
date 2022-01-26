0174e5b8f59ca0e9110b2be3f22b3933513e7ed9
Kashmir <kashmir.gaurav@amdocs.com>
2022-01-26 21:11:34 +0530

    Enter

---
+++ src/main/java/com/vytec/App.java
@@ -0,0 +1,13 @@
+package com.vytec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
